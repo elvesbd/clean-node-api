016add9149ee63587b2d7c072e861e4e07318cd4
elvesbd <elvesbd@yahoo.com>
2022-12-09 19:32:21 -0300

    refactor: rename class

--- src/data/usecases/add-survey/db-add-survey.ts
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -1,6 +1,6 @@
 import { AddSurvey, AddSurveyModel, AddSurveyRepository } from './db-add-survey-interfaces'
 
-export class DbAdSurvey implements AddSurvey {
+export class DbAddSurvey implements AddSurvey {
   constructor (private readonly addsurveyRepository: AddSurveyRepository) {}
   async add (data: AddSurveyModel): Promise<void> {
     await this.addsurveyRepository.add(data)
