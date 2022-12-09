550ceded16ecd0e24decbc1fcc5b4a3745c57675
elvesbd <elvesbd@yahoo.com>
2022-12-08 21:14:19 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase eith correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,9 +1,10 @@
 import { BadRequestException } from '../../../helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-interfaces'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-interfaces'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -11,6 +12,9 @@ export class AddSurveyController implements Controller {
     if (error) {
       return BadRequestException(error)
     }
+
+    const { answers, question } = httpRequest.body
+    await this.addSurvey.add({ answers, question })
     return undefined
   }
 }
