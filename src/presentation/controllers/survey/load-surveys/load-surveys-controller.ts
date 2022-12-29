15e302e606bf88640eebe0583a04ea56b6c80825
elvesbd <elvesbd@yahoo.com>
2022-12-29 19:39:11 -0300

    feat: ensure LoadSurveysController return 204 if LoadSurveys returns empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,4 @@
-import { Ok, ServerErrorException } from '../../../helpers/http/http-helper'
+import { Ok, NoContent, ServerErrorException } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller.interfaces'
 
 export class LoadSurveysController implements Controller {
@@ -6,7 +6,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return Ok(surveys)
+      return surveys.length ? Ok(surveys) : NoContent()
     } catch (error) {
       return ServerErrorException(error)
     }
