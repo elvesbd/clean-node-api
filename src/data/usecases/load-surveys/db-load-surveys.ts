e4bde36cd1ba0fe11c441408b2105f22d0f9719e
elvesbd <elvesbd@yahoo.com>
2022-12-29 19:56:26 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,12 @@
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveyRepository } from '../../interfaces/db/survey/load-survey-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveyRepository: LoadSurveyRepository) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveyRepository.loadAll()
+    return []
+  }
+}
