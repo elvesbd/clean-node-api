53264175886421e66a6a1621531339111ef00b85
elvesbd <elvesbd@yahoo.com>
2023-01-04 21:21:32 -0300

    refactor: change models to use Omit

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,6 @@
 import { AccountModel } from '@/domain/models/account'
 
-export interface AccountDTO {
-  name: string
-  email: string
-  password: string
-}
-
+export type AccountDTO = Omit<AccountModel, 'id'>
 export interface AddAccount {
   add: (account: AccountDTO) => Promise<AccountModel | null>
 }
