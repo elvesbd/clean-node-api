9fd547a1e26ed91c10b881ae2baacd780cc2db78
elvesbd <elvesbd@yahoo.com>
2023-01-13 20:19:17 -0300

    test: ensure SaveSurveyResultController calls LoadSurveyById with correct value

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.spec.ts
@@ -0,0 +1,53 @@
+import { SaveSurveyResultController } from './save-survey-resulr.controller'
+import { HttpRequest, LoadSurveyById } from './save-survey-result.interface'
+import { SurveyModel } from '@/domain/models/survey'
+
+interface SutTypes {
+  sut: SaveSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_survey_id'
+  }
+})
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ],
+  date: new Date()
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    async loadById (id: string): Promise<SurveyModel> {
+      return makeFakeSurvey()
+    }
+  }
+  return new LoadSurveyByIdStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = makeLoadSurveyById()
+  const sut = new SaveSurveyResultController(loadSurveyByIdStub)
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('SaveSurveyResult Controller', () => {
+  it('should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(makeFakeHttpRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
